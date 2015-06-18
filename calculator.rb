04128613629293c2e2c9d3781d5d09180854c6ba
Damon Friedberg <damonfrieds@gmail.com>
2015-06-18 16:42:58 +0000

    basic stuff, nothing works yet, mostly barebones

---
+++ calculator.rb
@@ -0,0 +1,59 @@
+def aska
+  puts "What is your starting number?"
+  a = gets.chomp.to_i
+end
+############
+def askb
+  puts "And what is your operating number?"
+  b = gets.chomp.to_i
+end
+############
+def add(a, b)
+  return a+b
+end
+#############
+def sub(a, b)
+  return a-b
+end
+#############
+def mlt(a, b)
+  return a*b
+end
+#############
+def div(a, b)
+  return a/b
+end
+#############
+def mod(a, b)
+  return a%b
+end
+#############
+def exp(a, b)
+  return a**b
+end
+#############
+input = ""
+while input != "exit"
+  puts "Hello! Welcome to Damon's Calculator.
+  To add two numbers, press 1.
+  To subtract two numbers, press 2.
+  To multiply two numbers, press 3.
+  To divide two numbers, press 4.
+  To take a modulo of two numbers, press 5.
+  To bring a number to an exponent, press 6.
+  To exit, type exit."
+  input = gets.chomp
+  case input
+    when "1"
+    add(aska, askb)
+    when "2"
+    sub(aska, askb)
+    when "3"
+    mlt(aska, askb)
+    when "4"
+    div(aska, askb)
+    when "5"
+    mod(aska, askb)
+    when "6"
+    exp(aska, askb)
+  end
